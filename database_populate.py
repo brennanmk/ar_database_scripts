3e937cfb21c6f7e0d6b6c5c5a40d5f0305d051ff
Brennan <brennanmk2200@gmail.com>
2022-10-27 11:54:28 -0400

    Added more table entries

--- database_populate.py
+++ database_populate.py
@@ -5,40 +5,51 @@ https://docs.python.org/3/library/csv.html
 '''
 
 from peewee import *
-import datetime
 import csv
 
 db = MySQLDatabase('ar', host='71.232.14.210', port=3307, user='root', password='NveQlG8bKp89hPWMdhdC6jBnd')
 
 class robots(Model):
-
     name = CharField()
-    created = DateField(default=datetime.date.today)
+    ip_addr = CharField()
+    image = CharField()
+    urdf = CharField()
+    twist_topic = CharField()
+    twist_speed = FloatField()
+    base_dof = IntegerField()
 
     class Meta:
 
         database = db
-        db_table = 'notes'
+        db_table = 'robots'
 
 class database_controller():
+    def __init__(self):
+        self.robots = robots()
+
     def create_tables(self):      
-        robots.create_table()
+        self.robots.create_table()
 
     def drop_tables(self):      
-        robots.drop_table()
+        self.robots.drop_table()
 
     def populate_tables(self):
-        data = []
         with open('data.csv') as csv_file:
-                reader = csv.reader(csv_file, delimiter=',')
-                for row in reader:
-                    print(', '.join(row))
+                data = csv.DictReader(csv_file)
 
-        for vals in data:
-            robots.create(text='').save()
+                for vals in data:
+                    self.robots.create(
+                        name= vals['robot_name'],
+                        ip_addr = vals['ros_master_uri'],
+                        image = vals['image_target_location'],
+                        urdf = vals['urdf_location'],
+                        base_dof = int(vals['base_dof']),
+                        twist_topic = vals['twist_topic'],
+                        twist_speed = float(vals['twist_speed']),
+                    ).save()
     
 if __name__ == '__main__':
-    controller = database_controller
+    controller = database_controller()
     controller.drop_tables()
     controller.create_tables()
     controller.populate_tables()
